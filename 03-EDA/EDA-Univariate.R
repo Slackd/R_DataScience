db1f8594e2b15684d0fbe029ef5ee3e29b937694
Budhaditya Saha <budha.rocks@gmail.com>
2018-12-07 05:09:20 +0530

    starting....

--- 03-EDA/EDA-Univariate.R
+++ 03-EDA/EDA-Univariate.R
@@ -1,3 +1,10 @@
+# Exploratory Data Analysis (EDA)
+
 # Set Working Directory ----
 setwd("~/dev/R_DataScience/03-EDA")
+
+# Univariate Analysis ----
+# Data Structures
+
+bike_buyers <- read.csv("../assets/csv/bike_buyers.csv")
 
